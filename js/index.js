63880807f7d4c58dd48cbda8a7822f14edc9b6a3
donellgill <donell.gill@outlook.com>
2021-04-18 21:05:37 -0400

    deliverables 1 and 2

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
-/*let users = [];
 
-*/
+
+
+/*create function to not load js content until html has loaded*/
 document.addEventListener("DOMContentLoaded", function() {
     console.log("The DOM has loaded");
   });
 
-
+  /*gets user object from api*/
 function fetchGitHubByValue(value){
 
     fetch(`https://api.github.com/search/users?q=${value}`,{
@@ -13,45 +14,37 @@ function fetchGitHubByValue(value){
             'Accept': 'application/vnd.github.v3+json'
         }
     })
-    .then(res => res.json())
-    .then(json => console.log(json))
-    
-}
+    .then(res => res.json()
+    )
+    .then(json => json.items.forEach((user) => {
+        const userLi= createLi(user)
+        
+    }));
 
+
+function addClick(){
     const input = document.getElementById('search')
     const submitBtn = document.getElementById('submit-button')
-    submitBtn.addEventListener('click', (e) => {
-        e.preventDefault()
+    submitBtn.addEventListener('click', function(event) {
+        event.preventDefault()
         fetchGitHubByValue(input.value)
+        
     })
-    
-   /* function(users){
-       .then(json => console.log(json))
+}
+   
+  function userLi(user) {
     let username = users.items.login;
     let avatar = users.items.avatar_url;
     let link = users.items.url;
 
-    console.log(username, avatar, link);
-})    */
+    return(username, avatar, link);
+}
 
 
-
+function createLi(user){
+    const li = document.createElement('li')
+    li.appendChild()
+}
     
-    
-
-
-/*   {
-  // Use this data inside of `json` to do DOM manipulation
-}
 
 }
-
-function listUser(){
-users = Object.keys()
-{
-var input = document.getElementById("userInput");
-alert(input);
-}
-
-*/
-
